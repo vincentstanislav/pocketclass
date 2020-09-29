526412663772d38eba3c21caaf03f7d334fe5df2
s.vincent <vincentstanislav@gmail.com>
2020-09-29 23:55:38 +0200

    toast and  onClickListener

--- app/src/main/java/com/gloxeer/pocketclass/MainActivity.java
+++ app/src/main/java/com/gloxeer/pocketclass/MainActivity.java
@@ -1,14 +1,36 @@
 package com.gloxeer.pocketclass;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    //declare variables
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.confirm);
+
+        button.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.confirm:
+                Toast.makeText(this,"test",Toast.LENGTH_SHORT).show();
+                break;
+        }
+
     }
 }
