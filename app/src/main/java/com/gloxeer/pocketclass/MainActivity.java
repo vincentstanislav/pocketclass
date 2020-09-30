769c68489cb0d5159fb5686a38e19e1d8a504dc5
s.vincent <vincentstanislav@gmail.com>
2020-09-30 23:13:55 +0200

    toolbar and button

--- app/src/main/java/com/gloxeer/pocketclass/MainActivity.java
+++ app/src/main/java/com/gloxeer/pocketclass/MainActivity.java
@@ -1,17 +1,22 @@
 package com.gloxeer.pocketclass;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.ButtonBarLayout;
+import androidx.appcompat.widget.Toolbar;
 import androidx.cardview.widget.CardView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     //declare variables
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,16 +26,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Button button = findViewById(R.id.confirm);
 
         button.setOnClickListener(this);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.settings:
 
+                return true;
+
+            case R.id.favorite:
+
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.confirm:
-                Toast.makeText(this,"test",Toast.LENGTH_SHORT).show();
+                //Toast.makeText(this, "test", Toast.LENGTH_SHORT).show();
+                Button confirm = (Button) findViewById(R.id.confirm);
+                confirm.setText("changes");
                 break;
         }
-
     }
 }
