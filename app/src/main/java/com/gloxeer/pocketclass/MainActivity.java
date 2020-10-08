a287eb51da8b49785477cae4165975f8ec7c9a7b
s.vincent <vincentstanislav@gmail.com>
2020-10-08 22:53:38 +0200

    images, clickable cardview, calling new intent

--- app/src/main/java/com/gloxeer/pocketclass/MainActivity.java
+++ app/src/main/java/com/gloxeer/pocketclass/MainActivity.java
@@ -1,5 +1,6 @@
 package com.gloxeer.pocketclass;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,37 +14,50 @@ import androidx.appcompat.widget.ButtonBarLayout;
 import androidx.appcompat.widget.Toolbar;
 import androidx.cardview.widget.CardView;
 
-//public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+//public class MainActivity extends AppCompatActivity {
 
         //declare variables
-    private Button button;
-    private TextView textView;
-    private CardView cardView;
+    private CardView mathCV;
+    private CardView czechCV;
+    private CardView quizCV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mathCV = (CardView) findViewById(R.id.math);
+        mathCV.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                startMath();
+            }
+        });
+        //mathCV.performClick();
+
+        czechCV = (CardView) findViewById(R.id.czech);
+        czechCV.setOnClickListener(this);
+        //czechCV.performClick();
+
          }
 
-  /* // @Override
+     private void startMath(){   //method to start math activity
+        Intent intent = new Intent(MainActivity.this, MathActivity.class);
+        startActivity(intent);
+     }
+
+   // @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.confirm:
-                //Toast.makeText(this, "test", Toast.LENGTH_SHORT).show();
-                button = (Button) findViewById(R.id.confirm);
-                button.setText("changes");
+            case R.id.math:
+                mathCV = (CardView) findViewById(R.id.math);
+                Toast.makeText(this, "Math", Toast.LENGTH_SHORT).show();
                 break;
-            case R.id.textView:
-                textView = (TextView) findViewById(R.id.textView);
-                textView.setText("Clicked!");
-                break;
-            case R.id.card_sample:
-                cardView = (CardView) findViewById(R.id.card_sample);
-                textView.setText("Not Clicked!");
+            case R.id.czech:
+                czechCV = (CardView) findViewById(R.id.czech);
+                Toast.makeText(this, "Czech", Toast.LENGTH_SHORT).show();
                 break;
         }
-    }*/
+    }
 }
