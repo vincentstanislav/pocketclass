0150740efb92cc395ed030dc50e9d245e47d5b17
s.vincent <vincentstanislav@gmail.com>
2020-10-10 01:01:12 +0200

    math multiplication activity, grid layout, cardView

--- app/src/main/java/com/gloxeer/pocketclass/MathActivity.java
+++ app/src/main/java/com/gloxeer/pocketclass/MathActivity.java
@@ -1,14 +1,34 @@
 package com.gloxeer.pocketclass;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MathActivity extends AppCompatActivity {
+
+    private CardView multiplicationCV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_math);
+
+        multiplicationCV = (CardView) findViewById(R.id.multiplication);
+        multiplicationCV.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                startMathMultiplication();
+            }
+        });
     }
+
+    //method to start math multiplication activity
+    private void startMathMultiplication(){
+        Intent intent = new Intent(MathActivity.this, MathMultiplication.class);
+        startActivity(intent);
+    }
+
 }
